42c7272a6bb902b7761de32efb016e9f843bf247
Miikka K <miikkak@tuta.io>
2024-01-12 23:27:13 -0600

    maps and setup

--- init.lua
+++ init.lua
@@ -1,7 +1,10 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+require("config.keymaps")
+require("config.set")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.termguicolors = true
+
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git",
@@ -16,10 +19,60 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require('lazy').setup({
 
-    { 'rose-pine/neovim',                 name = 'rose-pine' },
+    { 'rose-pine/neovim',  name = 'rose-pine' },
     { "tpope/vim-fugitive" },
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
+
+    {
+        "https://github.com/williamboman/mason.nvim",
+        dependencies = {
+            "https://github.com/williamboman/mason-lspconfig.nvim",
+            "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
+        },
+        config = function()
+            -- import mason
+            local mason = require("mason")
+
+            -- import mason-lspconfig
+            local mason_lspconfig = require("mason-lspconfig")
+
+            local mason_tool_installer = require("mason-tool-installer")
+
+            -- enable mason and configure icons
+            mason.setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            })
+
+            mason_lspconfig.setup({
+                -- list of servers for mason to install
+                ensure_installed = {
+                    "tsserver",
+                    "tailwindcss",
+                    "astro",
+                    "lua_ls",
+                    "volar",
+                    "jsonls",
+                    "efm",
+                },
+                -- auto-install configured servers (with lspconfig)
+                automatic_installation = true, -- not the same as ensure_installed
+            })
+
+            mason_tool_installer.setup({
+                ensure_installed = {
+                    "prettierd",
+                    "stylua",
+                    "eslint_d",
+                },
+            })
+        end,
+    },
+
     { -- LSP Support
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
@@ -47,7 +100,7 @@ require('lazy').setup({
             lsp.setup()
 
             local cmp = require("cmp")
-
+lo  
             cmp.setup({
                 mapping = {
                     ["<CR>"] = cmp.mapping.confirm({ select = false }),
@@ -71,8 +124,16 @@ require('lazy').setup({
         'neovim/nvim-lspconfig',
         dependencies = {
             { 'hrsh7th/cmp-nvim-lsp' },
+            {
+                "SmiteshP/nvim-navbuddy",
+                dependencies = {
+                    "SmiteshP/nvim-navic",
+                    "MunifTanjim/nui.nvim"
+                },
+                opts = { lsp = { auto_attach = true } }
+            }
         },
-       
+
     },
 
     { -- Autocompletion
@@ -82,7 +143,7 @@ require('lazy').setup({
         },
     },
 
-    { -- Use which-key to show lsp_zero default_keymaps
+    {
         'folke/which-key.nvim',
         event = "VeryLazy",
         config = function()
@@ -91,6 +152,7 @@ require('lazy').setup({
             )
         end,
     },
+
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.5',
@@ -102,20 +164,37 @@ require('lazy').setup({
             { "<C-p>",      "<cmd>Telescope git_files<cr>" }
         },
     },
-    {
-        'nvim-treesitter/nvim-treesitter',
-        'nvim-treesitter/playground',
-        build = ':TSUpdate',
-        config = function()
-            local configs = require('nvim-treesitter.configs')
 
-            configs.setup({
-                ensure_installed = 'all',
-                sync_install = false,
+    {
+        "https://github.com/nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        build = ":TSUpdate",
+        dependencies = {
+            "https://github.com/windwp/nvim-ts-autotag",
+        },
+        config = function()
+            local treesitter = require("nvim-treesitter.configs")
+
+            treesitter.setup({
+                highlight = { enable = true },
+                indent = { enable = true },
+                autotag = { enable = true },
                 auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false
+                ensure_installed = {
+                    "lua",
+                    "css",
+                    "scss",
+                    "html",
+                    "vue",
+                    "javascript",
+                    "json",
+                    "typescript",
+                    "tsx",
+                    "astro",
+                    "markdown",
+                    "markdown_inline",
+                    "gitignore",
+                    "gitcommit",
                 },
             })
         end,
@@ -144,13 +223,48 @@ require('lazy').setup({
             { "<leader>4", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 4", },
             { "<leader>5", function() require("harpoon"):list():select(5) end, desc = "harpoon to file 5", },
         },
-    }
+    },
+
+    {
+        'voldikss/vim-floaterm', -- terminal windows management
+        config = function()
+            vim.g.floaterm_autoclose = true
+            vim.g.floaterm_opener = 'edit'
+            vim.g.floaterm_rootmarkers = { '.project', '.git', '.hg', '.svn', '.root', '.gitignore' }
+            vim.g.floaterm_width = 0.8
+            vim.g.floaterm_shell = vim.o.shell
+            vim.cmd([[
+				function s:floatermSettings()
+					" setlocal notermguicolors
+				endfunction
+				autocmd FileType floaterm call s:floatermSettings()
+			]])
+        end
+    },
+
+    {
+        'folke/trouble.nvim', -- list lsp diagnostics
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('trouble').setup({
+                mode = "document_diagnostics"
+            })
+        end
+    },
+
+
+    {
+        'Raimondi/delimitMate', -- automatic closing of surroundings in insert mode
+        config = function()
+            vim.g.delimitMate_expand_cr = 2
+            vim.g.delimitMate_expand_space = true
+            vim.g.delimitMate_matchpairs = "(:),[:],{:},<:>"
+            vim.cmd([[au FileType html,xml let b:delimitMate_matchpairs = "(:),[:],{:}"]])
+        end
+    },
 
 })
 
-
-require("config.keymaps")
-require("config.set")
 
 -- Use lsp_zero to manage lsp attachments.
 local lsp_zero = require('lsp-zero')
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,17 +9,23 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
--- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+vim.keymap.set("t", "<ESC>", "<C-\\><C-n>")
+
+-- Floating Terminal
+vim.keymap.set("n", "<C-/>", ":FloatermToggle<CR>")
+vim.keymap.set("t", "<C-/>", "<C-\\><C-n>:FloatermHide<CR>")
+vim.keymap.set("t", "<C-,>", "<C-\\><C-n>:FloatermPrev")
+vim.keymap.set("t", "<C-.>", "<C-\\><C-n>:FloatermNext")
+
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
 vim.keymap.set("i", "kj", "<Esc>")
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
@@ -32,9 +38,6 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
